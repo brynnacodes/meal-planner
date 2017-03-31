6b79dc31743f2fee09d5e3fed8abe4f45ccfe28c
Brynna <brynna.mccarter@gmail.com>
2017-03-30 21:38:26 -0700

    add Meal class constructor and simple display

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1>Meal Tracker</h1>
+    <h3>Today's date: {{month}}/{{day}}/{{year}}</h3>
+    <ul> Meal
+      <li>Name: {{firstMeal.name}}</li>
+      <li>Details: {{firstMeal.details}}</li>
+      <li>Calories: {{firstMeal.calories}}</li>
+      <li>Day: {{firstMeal.day}}</li>
+    </ul>
+  </div>
   `
 })
 
-export class AppComponent { }
+export class AppComponent {
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();this;
+
+  firstMeal: Meal = {
+    name: 'lasagna',
+    details: 'sausage, noodles, basil, marinara',
+    calories: 336,
+    day: 'Thursday'
+  }
+}
+
+  class Meal {
+    constructor(public name: string, public details: string, public calories: number, public day: string) { }
+  }
